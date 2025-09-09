23a219e76f650c57ec46695096e579dbe5e7df8f
Gregory Mitchell <me@gmitch215.xyz>
2025-09-09 02:13:13 +0000

    fix: add cors handler

---
+++ src/EventSubscriber/CorsSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\mantle2\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+
+class CorsSubscriber implements EventSubscriberInterface
+{
+	public static function getSubscribedEvents()
+	{
+		return [
+			KernelEvents::RESPONSE => 'onRespond',
+		];
+	}
+
+	public function onRespond(ResponseEvent $event)
+	{
+		$request = $event->getRequest();
+		$response = $event->getResponse();
+		$host = $request->headers->get('Origin') ?? $request->headers->get('Host');
+		if ($host === null) {
+			$host = '*'; // Fallback to wildcard if no host is provided
+		}
+
+		$response->headers->set('Access-Control-Allow-Origin', $host);
+		$response->headers->set(
+			'Access-Control-Allow-Methods',
+			'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+		);
+		$response->headers->set(
+			'Access-Control-Allow-Headers',
+			'Content-Type, Authorization, Accept, X-Requested-With, X-Admin-Key',
+		);
+		$response->headers->set('Access-Control-Allow-Credentials', 'true');
+		$response->headers->set('Access-Control-Max-Age', '3600');
+
+		$event->setResponse($response);
+	}
+}
