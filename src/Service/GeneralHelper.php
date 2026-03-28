ba31b7d4f47d0769f11c2e6819be93e185f36092
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-03-28 17:20:42 -0500

    fix: remove ttl from `motd_cycle_index`

--- src/Service/GeneralHelper.php
+++ src/Service/GeneralHelper.php
@@ -703,13 +703,9 @@ class GeneralHelper
 
 		RedisHelper::set('motd', $motd, 3600); // set MOTD with 1 hour TTL (cron hourly)
 		$nextIndex = ($selectedIndex + 1) % $cycleCount;
-		RedisHelper::set(
-			'motd_cycle_index',
-			[
-				'value' => $nextIndex,
-			],
-			3600,
-		);
+		RedisHelper::set('motd_cycle_index', [
+			'value' => $nextIndex,
+		]);
 
 		Drupal::logger('mantle2')->info('[cron] cycled motd to index @index: @motd', [
 			'@index' => $selectedIndex,
